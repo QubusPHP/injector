34319bec5bfc41a486d8329e2fc5d4e40f754b02
Joshua Parker <joshua@joshuaparker.dev>
2023-08-27 00:19:11 -0600

    Removed header from tests.

--- Tests/ServiceProvider/FakeServiceProvider.php
+++ Tests/ServiceProvider/FakeServiceProvider.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Qubus\Injector
- *
- * @link       https://github.com/QubusPHP/injector
- * @copyright  2021 Joshua Parker <josh@joshuaparker.blog>
- * @license    https://opensource.org/licenses/mit-license.php MIT License
- *
- * @since      2.0.0
- */
-
 declare(strict_types=1);
 
 namespace Qubus\Injector\Tests\ServiceProvider;
--- Tests/ServiceProviderTest.php
+++ Tests/ServiceProviderTest.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Qubus\Injector
- *
- * @link       https://github.com/QubusPHP/injector
- * @copyright  2021 Joshua Parker <josh@joshuaparker.blog>
- * @license    https://opensource.org/licenses/mit-license.php MIT License
- *
- * @since      2.0.0
- */
-
 namespace Qubus\Tests\Injector;
 
 use PHPUnit\Framework\Assert;
